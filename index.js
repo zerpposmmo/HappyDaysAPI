39595ae4d4105a8513bda6dc7af41faf322f8b34
Arnaud <arnaud.bernard97@gmail.com>
2018-05-29 21:44:39 +0200

    fix(api): added cors to allow all origins

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ var express = require('express'),
     port = process.env.PORT || 3000;
 
 var controller = require('./controller');
+
+var cors = require('cors');
+app.use(cors({
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'origin': '*',
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+}));
 app.use('/api', controller);
 app.listen(port);
 
