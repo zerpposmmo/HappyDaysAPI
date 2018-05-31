db0a5124cc9006a514858d89d749b40639ec36c7
Arnaud <arnaud.bernard97@gmail.com>
2018-05-31 09:06:48 +0200

    fix(controller): refactored route management

--- controller.js
+++ controller.js
@@ -1,5 +1,4 @@
 var jinst = require('jdbc/lib/jinst');
-
 if (!jinst.isJvmCreated()) {
     jinst.addOption("-Xrs");
     jinst.setupClasspath([
@@ -16,122 +15,73 @@ var config = {
         password: 'pass'
     }
 };
-
 var jdbc = new (require('jdbc'))(config);
-
-var asyncjs = require('async');
-
 jdbc.initialize(function (err) {
     if (err) {
         console.log(err);
     }
 });
 
+var asyncjs = require('async');
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-router.use(bodyParser.urlencoded({ extended: true }));
 
-router.get('/tours', function (req, res) {
-    jdbc.reserve(function (err, connObj) {
-        asyncjs.series([function (callback) {
-            var conn = connObj.conn;
-            conn.createStatement(function (err, statement) {
-                if (err) {
-                    res.status(400).send(err);
-                } else {
-                    statement.executeQuery("SELECT ID, POIDSMAX, VOLUMEMAX, COMMANDE_ID, TOURNEE_ID FROM COLIS",
-                        function (err, resultset) {
-                            if (err) {
-                                res.status(400).send(err);
-                            } else {
-                                resultset.toObjArray(function (err, data) {
-                                    if (!isEmptyObject(data)) {
-                                        data = groupBy(data, "TOURNEE_ID");
-                                        res.status(200).send(data);
-                                    }
-                                    callback(null, resultset);
-                                });
-                            }
-                        });
-                }
-            });
-        }], function (err, res) {
-            jdbc.release(connObj, function (err) {
-                if (err) {
-                    res.status(400).send(err);
-                }
+var sql = require('./sql.json');
+router.use(bodyParser.urlencoded({extended: true}));
+
+router.get('*', function (req, res) {
+    var pathname = req.originalUrl.replace('/api','');
+    var request = '';
+    switch (pathname) {
+        case '/tours':
+            request = sql.tours;
+            break;
+        case '/orders':
+            request = sql.orders;
+            break;
+        case 'products':
+            request = sql.products;
+            break;
+        default:
+            break;
+    }
+    if (request !== '') {
+        jdbc.reserve(function (err, connObj) {
+            asyncjs.series([function (callback) {
+                var conn = connObj.conn;
+                conn.createStatement(function (err, statement) {
+                    if (err) {
+                        res.status(400).send(err);
+                    } else {
+                        statement.executeQuery(request,
+                            function (err, resultset) {
+                                if (err) {
+                                    res.status(400).send(err);
+                                } else {
+                                    resultset.toObjArray(function (err, data) {
+                                        if (!isEmptyObject(data)) {
+                                            res.status(200).send(data);
+                                        }
+                                        callback(null, resultset);
+                                    });
+                                }
+                            });
+                    }
+                });
+            }], function (err, res) {
+                jdbc.release(connObj, function (err) {
+                    if (err) {
+                        res.status(400).send(err);
+                    }
+                });
             });
         });
-    });
-});
+    } else {
+        res.status(404).send('Unknown route ' + pathname);
+    }
 
-router.get('/products', function (req, res) {
-    jdbc.reserve(function (err, connObj) {
-        asyncjs.series([function (callback) {
-            var conn = connObj.conn;
-            conn.createStatement(function (err, statement) {
-                if (err) {
-                    res.status(400).send(err);
-                } else {
-                    statement.executeQuery("SELECT * FROM PRODUIT",
-                        function (err, resultset) {
-                            if (err) {
-                                res.status(400).send(err);
-                            } else {
-                                resultset.toObjArray(function (err, data) {
-                                    if (!isEmptyObject(data)) {
-                                        res.status(200).send(data);
-                                    }
-                                    callback(null, resultset);
-                                });
-                            }
-                        });
-                }
-            });
-        }], function (err, res) {
-            jdbc.release(connObj, function (err) {
-                if (err) {
-                    res.status(400).send(err);
-                }
-            });
-        });
-    });
 });
-
-router.get('/orders', function (req, res) {
-    jdbc.reserve(function (err, connObj) {
-        asyncjs.series([function (callback) {
-            var conn = connObj.conn;
-            conn.createStatement(function (err, statement) {
-                if (err) {
-                    res.status(400).send(err);
-                } else {
-                    statement.executeQuery("SELECT * FROM COMMANDE",
-                        function (err, resultset) {
-                            if (err) {
-                                res.status(400).send(err);
-                            } else {
-                                resultset.toObjArray(function (err, data) {
-                                    if (!isEmptyObject(data)) {
-                                        res.status(200).send(data);
-                                    }
-                                    callback(null, resultset);
-                                });
-                            }
-                        });
-                }
-            });
-        }], function (err, res) {
-            jdbc.release(connObj, function (err) {
-                if (err) {
-                    res.status(400).send(err);
-                }
-            });
-        });
-    });
-});
-
 module.exports = router;
 
 function isEmptyObject(obj) {
@@ -144,8 +94,8 @@ function isEmptyObject(obj) {
 }
 
 
-var groupBy = function(xs, key) {
-    return xs.reduce(function(rv, x) {
+var groupBy = function (xs, key) {
+    return xs.reduce(function (rv, x) {
         (rv[x[key]] = rv[x[key]] || []).push(x);
         return rv;
     }, {});
